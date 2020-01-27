1328b77343a706be514dcb09e11d1597ccf5eacd
Vitalii <vmatviishyn@gmail.com>
2020-01-27 16:46:19 +0200

    Router State and Entity Composition

--- src/products/store/reducers/index.ts
+++ src/products/store/reducers/index.ts
@@ -1,4 +1,4 @@
-import { ActionReducerMap, createFeatureSelector, createSelector } from '@ngrx/store';
+import { ActionReducerMap, createFeatureSelector } from '@ngrx/store';
 
 import * as fromPizzas from './pizzas.reducer';
 
@@ -10,23 +10,4 @@ export const reducers: ActionReducerMap<ProductsState> = {
     pizzas: fromPizzas.reducer
 };
 
-// selectors
 export const getProductsState = createFeatureSelector<ProductsState>('products');
-
-// pizzas state
-export const getPizzasState = createSelector(
-    getProductsState,
-    (state: ProductsState) => state.pizzas
-);
-
-export const getPizzasEntities = createSelector(getPizzasState,fromPizzas.getPizzasEntities);
-
-export const getAllPizzas = createSelector(
-    getPizzasEntities,
-    (entities) => {
-        return Object.keys(entities).map(id => entities[parseInt(id, 10)]);
-    }
-);
-
-export const getPizzasLoaded = createSelector(getPizzasState,fromPizzas.getPizzasLoaded);
-export const getPizzasLoading = createSelector(getPizzasState,fromPizzas.getPizzasLoading);
