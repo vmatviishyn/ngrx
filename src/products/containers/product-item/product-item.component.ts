f40d41a5df019b620c9fcfd85938ef5582b841de
Vitalii <vmatviishyn@gmail.com>
2020-01-29 13:25:25 +0200

    Store Selectors and Async Pipe

--- src/products/containers/product-item/product-item.component.ts
+++ src/products/containers/product-item/product-item.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import * as fromStore from '../../store';
 
 import { Pizza } from '../../models/pizza.model';
@@ -14,27 +15,27 @@ import { Topping } from '../../models/topping.model';
 })
 export class ProductItemComponent implements OnInit {
   pizza$: Observable<Pizza>;
-  visualise: Pizza;
+  visualise$: Observable<Pizza>;
   toppings$: Observable<Topping>;
 
   constructor(private store: Store<fromStore.ProductsState>) {}
 
   ngOnInit() {
-    this.store.dispatch(new fromStore.LoadToppings());
-    this.pizza$ = this.store.select(fromStore.getSelectedPizza);
-    this.toppings$ = this.store.select(fromStore.getAllToppings)
+    this.pizza$ = this.store.select(fromStore.getSelectedPizza).pipe(
+      tap((pizza: Pizza = null) => {
+        const pizzaExists = !!(pizza && pizza.toppings);
+        const toppings = pizzaExists
+          ? pizza.toppings.map(topping => topping.id)
+          : [];
+        this.store.dispatch(new fromStore.VisualizeToppings(toppings));
+      })
+    );
+    this.toppings$ = this.store.select(fromStore.getAllToppings);
+    this.visualise$ = this.store.select(fromStore.getPizzaVisualized);
   }
 
   onSelect(event: number[]) {
-    // let toppings;
-    // if (this.toppings && this.toppings.length) {
-    //   toppings = event.map(id =>
-    //     this.toppings.find(topping => topping.id === id)
-    //   );
-    // } else {
-    //   toppings = this.pizza.toppings;
-    // }
-    // this.visualise = { ...this.pizza, toppings };
+    this.store.dispatch(new fromStore.VisualizeToppings(event))
   }
 
   onCreate(event: Pizza) {
--- src/products/containers/products/products.component.ts
+++ src/products/containers/products/products.component.ts
@@ -19,5 +19,6 @@ export class ProductsComponent implements OnInit {
   ngOnInit() {
     this.pizzas$ = this.store.select<any>(fromStore.getAllPizzas);
     this.store.dispatch(new fromStore.LoadPizzas());
+    this.store.dispatch(new fromStore. LoadToppings());
   }
 }
